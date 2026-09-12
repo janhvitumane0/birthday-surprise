e77c572d87825c6ce0e6085238079c474e013331
janhvitumane0 <janhvitumane0@gmail.com>
2026-09-12 13:36:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -320,7 +320,13 @@ if (whoAnswerBtn && whoAnswer && whoContinue) {
     });
   };
 }
-
+<button
+  class="continue-btn"
+  id="who-continue"
+  data-next="final-screen"
+  hidden>
+  Continue to the end ♡
+</button>
 /* Keyboard convenience */
 document.addEventListener("keydown",e=>{
   if(e.key==="Escape")closeModal();
